fd6882a182477a2e0528ec997930ad3fee53771e
Jehan Perera <jehan.perera@ukg.com>
2022-07-13 17:36:38 +0530

    Add Context

---
+++ src/context/BlogContext.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({children}) => {
+    const [blogPosts, setBlogPosts] = useState([
+        {title: 'Blog Post #1'}
+    ]);
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, {
+            title: `Blog Post #${blogPosts.length + 1}`
+        }]);
+    }
+
+    return (
+        <BlogContext.Provider value={{
+            data: blogPosts,
+            addBlogPost,
+        }}>
+            {children}
+        </BlogContext.Provider>
+    );
+}
+
+export default BlogContext;
