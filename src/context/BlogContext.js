155ee6ceef4fafb8deffd07091257e919282fea2
Jehan Perera <jehan.perera@ukg.com>
2022-07-13 19:58:06 +0530

    Add delete function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,11 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
     switch(action.type){
         case 'add_blog_post':
-            return [...state, {title: `Blog Post #${state.length+1}`}];
+            return [...state, { 
+                id: Math.floor(Math.random()*999999999), 
+                title: `Blog Post #${state.length+1}`}];
+        case 'delete_blog_post':
+            return state.filter((blogPost)=>blogPost.id != action.payload);
         default:
             return state;
     }
@@ -15,8 +19,17 @@ const addBlogPost = (dispatch) => {
     });
 }
 
+const deleteBlogPost = (dispatch) =>{
+    return (id) =>{
+        dispatch({
+            type: 'delete_blog_post', 
+            payload: id,
+        });
+    };
+};
+
 export const {Context, Provider} = createDataContext(
     blogReducer, 
-    {addBlogPost}, 
+    {addBlogPost,deleteBlogPost}, 
     []
 );
